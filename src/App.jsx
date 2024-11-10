5e78d5b07416ac10e7e4226002403ea50c08234d
Bappi Saha <bappi35-2097@diu.edu.bd>
2024-11-10 11:27:28 +0600

    full page size fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
-import "./App.css";
-import ImageGallery from "./ImageGalary";
-import animals from "./data/animal.json";
-import AnimalAwarenessCard from "./AnimalCard";
-import Header from "./components/header";
+import "./App.css"
+import ImageGallery from "./ImageGalary"
+import animals from "./data/animal.json"
+import AnimalAwarenessCard from "./AnimalCard"
+import Header from "./components/header"
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <div className="container"></div>
         <AnimalAwarenessCard />
         <ImageGallery animals={animals} />
       </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
